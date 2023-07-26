2a2b834368491671d3fe29794630b3b6a34a0b81
Muhammed Ekici <ekcmuhammed08@gmail.com>
2023-07-26 19:14:24 +0300

    added logo

--- web3-frontend/src/App.tsx
+++ web3-frontend/src/App.tsx
@@ -54,7 +54,7 @@ function App() {
                 />
               </a>
             </div>
-
+            
             <div className="flex items-center space-x-4">
               <WalletInfo/>
             </div>
@@ -85,7 +85,10 @@ function App() {
                     </div>
                     <div
                       className="mt-5 relative block w-full border-2 border-gray-300 border-dashed rounded-lg p-12 text-center focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                        {/* <div className="flex flex-col items-center"><HiOutlineCollection size={'48px'}/></div> */}
+                      <div className="flex flex-col items-center">
+                        <svg width="64px" height="64px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="none"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g fill="#000000"> <path d="M3.05 4.75a.7.7 0 01.7-.7h3.5a.7.7 0 110 1.4h-3.5a.7.7 0 01-.7-.7zM3.75 7.05a.7.7 0 100 1.4h2.5a.7.7 0 100-1.4h-2.5z"></path> <path fill-rule="evenodd" d="M0 3.25A2.25 2.25 0 012.25 1h6.5c.883 0 1.648.51 2.016 1.25h.984c.698 0 1.3.409 1.582 1h.918c.966 0 1.75.784 1.75 1.75v6a1.75 1.75 0 01-1.75 1.75h-.918c-.281.591-.884 1-1.582 1h-.984A2.25 2.25 0 018.75 15h-6.5A2.25 2.25 0 010 12.75v-9.5zm13.5 8h.75a.25.25 0 00.25-.25V5a.25.25 0 00-.25-.25h-.75v6.5zm-2.5 1v-8.5h.75A.25.25 0 0112 4v8a.25.25 0 01-.25.25H11zM2.25 2.5a.75.75 0 00-.75.75v9.5c0 .414.336.75.75.75h6.5a.75.75 0 00.75-.75v-9.5a.75.75 0 00-.75-.75h-6.5z" clip-rule="evenodd"></path> </g> </g>
+                        </svg>
+                      </div>
                       <span
                         className="mt-2 block text-sm font-medium text-gray-900"> No open cohorts to participate on. </span>
                     </div>
