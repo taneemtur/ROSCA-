ef333438c53c88d2557032461782882b40333b4b
Muhammed Ekici <ekcmuhammed08@gmail.com>
2023-07-26 13:15:43 +0300

    fixed

--- web3-frontend/src/components/RoscaCard.tsx
+++ web3-frontend/src/components/RoscaCard.tsx
@@ -1,97 +1,99 @@
 
 import { TezosToolkit } from '@taquito/taquito'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useAppName, useContractAddress, useEndpoint, useNetwork } from '../contexts/Settings'
+import { FaUserTie } from 'react-icons/fa'
 
 const RoscaCard = (props:any) => {
-
     const name = useAppName()
     const network = useNetwork()
     const endpoint = useEndpoint()
     const contractAddress = props.contract
     const [info,setInfo] = useState(false)
-    
-    const ContractData = (props:any) => {
-        const handleClick = () =>{
-            loadStorage()
-        }
-        const [{owner,rosca_total,contributing_duration,
-            max_participants,participant,paused,pot,participants_count,
-            contributors_count,contribute_amount,end_time,receiver,admin},setData] = useState(()=>({
-            owner:null,
-            rosca_total:null,
-            contributing_duration:null,
-            max_participants:null,
-            participant:null,
-            paused: false,
-            pot:null,
-            participants_count:null,
-            contributors_count:null,
-            contribute_amount:null,
-            end_time:null,
-            receiver:null,
-            admin:null,      
-        }))
-        const [storage,setStorage] = useState(null)
-        const loadStorage = React.useCallback(async()=>{
-          const tezos = new TezosToolkit(endpoint)
-          const contract = await tezos.contract.at(contractAddress)
-          const contractStorage: any = await contract.storage()
-          setData({
-            owner: contractStorage.owner,
-            rosca_total: contractStorage.rosca_total, 
-            contributing_duration: contractStorage.contributing_duration,
-            max_participants: contractStorage.max_participants,
-            participant: contractStorage.participant,
-            paused: contractStorage.paused,
-            pot: contractStorage.pot, 
-            participants_count: contractStorage.participants_count,
-            contributors_count: contractStorage.contributors_count,
-            contribute_amount: contractStorage.contribute_amount,
-            end_time: contractStorage.end_time,
-            receiver: contractStorage.receiver,
-            admin: contractStorage.admin,
-          }) 
-          setStorage(contractStorage)
-        },[])
-        console.log("Owner: ",JSON.stringify(owner)) 
-        console.log("Total Rosca: ",JSON.stringify(rosca_total)) 
-        console.log("Duration: ",JSON.stringify(contributing_duration)) 
-        console.log("Max Users: ",JSON.stringify(max_participants)) 
-        console.log("Participants Array: ",JSON.stringify(participant)) 
-        console.log("Is Paused: ",JSON.stringify(paused)) 
-        console.log("Pot: ",JSON.stringify(pot)) 
-        console.log("Participants Count: ",JSON.stringify(participants_count)) 
-        console.log("Contributors Count: ",JSON.stringify(contributors_count)) 
-        console.log("Contribute Amount: ",JSON.stringify(contribute_amount)) 
-        console.log("End Time: ",JSON.stringify(end_time)) 
-        console.log("Receiver: ",JSON.stringify(receiver))  
-        console.log("Admin: ",JSON.stringify(admin)) 
-        return (
-        <div> <button style={{background:'aqua', padding:'10px', margin:'8px'}} onClick={handleClick}>GetInfo</button> 
-            <div> 
-                <p>Owner: {owner && JSON.stringify(owner)}</p>
-                <p>Total Rosca: {rosca_total && JSON.stringify(rosca_total)}</p>
-                <p>Duration: {contributing_duration && JSON.stringify(contributing_duration)}</p>
-                <p>Max Users: {max_participants && JSON.stringify(max_participants)}</p>
-                <p>Participants Array: {participant && JSON.stringify(participant)}</p>
-                <p>Is Paused: {paused && Boolean(paused)}</p>
-                <p>Pot: {pot && JSON.stringify(pot)}</p>
-                <p>Participants Count: {participants_count && JSON.stringify(participants_count)}</p>
-                <p>Contributors Count: {contributors_count && JSON.stringify(contributors_count)}</p>
-                <p>Contribute Amount: {contribute_amount && JSON.stringify(contribute_amount)}</p>
-                <p>End Time: {end_time && JSON.stringify(end_time)}</p>
-                <p>Receiver: {receiver && JSON.stringify(receiver)}</p>
-                <p>Admin: {admin && JSON.stringify(admin)}</p>
-            </div>
-        </div>
-        )
-    }
-  return (
-    <div>
-      <ContractData/>
-    </div>
-  )
-}
 
+    const [{owner,rosca_total,contributing_duration,
+        max_participants,participant,paused,pot,participants_count,
+        contributors_count,end_time,receiver,admin},setData] = useState<any>(()=>({
+        owner:null,
+        rosca_total:null,
+        contributing_duration:null,
+        max_participants:null,
+        participant:null,
+        paused: null,
+        pot:null,
+        participants_count:null,
+        contributors_count:null,
+        end_time:null,
+        receiver:null,
+        admin:null,      
+    }))
+    const [storage,setStorage] = useState(null)
+    const loadStorage = React.useCallback(async()=>{
+        const tezos = new TezosToolkit(endpoint)
+        const contract = await tezos.contract.at(contractAddress)
+        const contractStorage: any = await contract.storage()
+        setData({
+        owner: contractStorage.owner,
+        rosca_total: contractStorage.rosca_total/1000000, 
+        contributing_duration: contractStorage.contributing_duration,
+        max_participants: contractStorage.max_participants,
+        participant: contractStorage.participant,
+        paused: contractStorage.paused,
+        pot: contractStorage.pot, 
+        participants_count: contractStorage.participants_count,
+        contributors_count: contractStorage.contributors_count,
+        end_time: contractStorage.end_time,
+        receiver: contractStorage.receiver,
+        admin: contractStorage.admin,
+        }) 
+        setStorage(contractStorage)
+    },[])
+    
+    // console.log("Owner: ",JSON.stringify(owner)) 
+    // console.log("Total Rosca: ",JSON.stringify(rosca_total)) 
+    // console.log("Duration: ",JSON.stringify(contributing_duration)) 
+    // console.log("Max Users: ",max_participants)
+    // console.log("Is Paused: ", paused ) 
+    // console.log("Pot: ",pot) 
+    // console.log("Participants Count: ",JSON.stringify(participants_count)) 
+    // console.log("Contributors Count: ",JSON.stringify(contributors_count))  
+    // console.log("End Time: ",JSON.stringify(end_time)) 
+    // console.log("Receiver: ",JSON.stringify(receiver))  
+    // console.log("Admin: ",JSON.stringify(admin)) 
+    // const parsed = JSON.parse(JSON.stringify(participant))
+    
+    useEffect(() => {
+        loadStorage()
+    }, [])
+    
+    return (
+    <div> 
+        {/* <button style={{background:'aqua', padding:'10px', margin:'8px'}} onClick={handleClick}>GetInfo</button>  */}
+        <div className='bg-[#EBEBEB] m-16 w-[380px] h-64  rounded-[48px] border border-black'> 
+            <div className='flex flex-row justify-between bg-[#09417D] w-full h-20 pr-6 pl-10 pt-4 rounded-t-[48px]'>
+                <div className="text-xl text-white">
+                    <p>Rosca: {contractAddress.slice(0,6)+ "........" +contractAddress.slice(contractAddress.length-7,contractAddress.length)}</p>
+                    <p>Collecting Applications...</p>
+                </div>
+                <div className="bg-[#24FF00] mt-2 h-10 w-10 rounded-full"></div>
+            </div>
+            <div className="flex flex-col h-32 pr-6 pl-12 pt-6">
+                <div className='flex flex-row pb-2'>
+                    <div className="pt-1"><FaUserTie/></div>
+                    <p className='pl-4'>sdfsdfsdf23423fw2fd23d</p>
+                </div>
+                <div className='flex flex-row'>
+                    <div className="pt-1"><FaUserTie/></div>
+                    <p className='pl-4'>sdfsdfsdf23423fw2fd23d</p>
+                </div>
+                <div className='flex flex-row'>
+                    <div className="pt-1"><FaUserTie/></div>
+                    <p className='pl-4'>sdfsdfsdf23423fw2fd23d</p>
+                </div>
+            </div>
+            <div className="bg-[#D9D9D9] w-full h-12 pr-6 pl-10 rounded-b-[48px] -mt-[2px] border">b</div>
+        </div>
+    </div>
+    )
+}
 export default RoscaCard
